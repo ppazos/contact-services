0120438e6e75d81926b060a0d06e7ff0a0ab54ea
Pablo Pazos <pablo.pazos@cabolabs.com>
2016-06-19 05:21:47 -0300

    added basic functions and tests

--- ContactInformation.groovy
+++ ContactInformation.groovy
@@ -1,5 +1,10 @@
 package contact_services
 
+// TODO: with a dictionary of names we can guess the names and put the rest on the lastname.
+
+// EqualsAndHashCode allows to implement the minus as a -
+// equals just by email excludes name and lastname fields http://docs.groovy-lang.org/latest/html/api/groovy/transform/EqualsAndHashCode.html
+@groovy.transform.EqualsAndHashCode(excludes=["name","lastname"])
 class ContactInformation {
 
    String email
@@ -8,7 +13,7 @@ class ContactInformation {
    
    
    /**
-    * contactString has the form contact, contact, contact, ...
+    * contact_string has the form contact<line separator>contact<line separator>contact<line separator>...
     * where contact can be:
     * - email
     * - name <email>
@@ -24,41 +29,72 @@ class ContactInformation {
     *
     * and between contact there can be any number of whitespaces or new lines
     */
-   static List getContactInformationFromString(contactString)
+   static List getContactInformationFromString(contact_string)
    {
       def res = []
-      def contactList = contactString.split(",")*.trim()
+      
+      // split just by line separator, so is one contact per line!
+      // denormalize transforms all end of lines to the platform end of line
+      def contactList = contact_string.denormalize().split(System.getProperty("line.separator"))*.trim()
+      
+      // THE INPUT SHOULD REMOVE , because , can be also part of the name...
+      // with tokenize allows to have newlines between contact information or commas
+      //def contactList = contact_string.tokenize(",\n")*.trim()
+      //contactList.removeAll([""]) // if a line has , and newline it generates an empty string elements on the list
+      //println contactList
       
       def contactTokens
       contactList.each { contact ->
          contactTokens = contact.split(" ")
          
          // TODO: verify valid emails by regex: http://stackoverflow.com/questions/8204680/java-regex-email
+         // TODO: report invalid emails
          // TODO: if name is email (case a@a.a <a@a.a>), avoid putting it in the name
          // TODO: if there are more than 5 parts, join all together in the name
          switch (contactTokens.size())
          {
             case 1:
-              res << new ContactInformation(email: contactTokens[0])
+              res << new ContactInformation(email: contactTokens[0].toLowerCase())
             break
             case 2:
-              res << new ContactInformation(name: contactTokens[0].replaceAll('"',''), email: contactTokens[1].replaceAll('<','').replaceAll('>',''))
+              def name = contactTokens[0].replaceAll('"','')
+              def email = contactTokens[1].toLowerCase().replaceAll('<','').replaceAll('>','')
+              if (name == email) name = null // TODO: we can try to guess the name from the email user part like check if it is name.lastname
+              res << new ContactInformation(name: name,
+                                            email: email)
             break
             case 3:
               res << new ContactInformation(name: contactTokens[0].replaceAll('"',''),
                                             lastname: contactTokens[1].replaceAll('"',''), 
-                                            email: contactTokens[2].replaceAll('<','').replaceAll('>',''))
-            break
-            case 4:
-            break
-            case 5:
+                                            email: contactTokens[2].toLowerCase().replaceAll('<','').replaceAll('>',''))
             break
             default:
-               throw Exception("contact has "+ contactTokens.size() + " parts")
+               //println "++"+ contactTokens +" ++ "+ contactTokens[contactTokens.size()-1]
+               
+               def count = contactTokens.size()
+               
+               def email = contactTokens[count-1].toLowerCase().replaceAll('<','').replaceAll('>','') // TODO: check format!
+               def name = "" // join the rest of the tokens in the name because we can't guess what is what
+               for (int i in 0..count-2)
+               {
+                  name += contactTokens[i].replaceAll('"','') +" "
+               }
+               name = name[0..-2] // removes last space
+               
+               //println email
+               
+               res << new ContactInformation(name: name, email: email)
          }
       }
       
       return res
+   }
+   
+   static boolean isValidEmail(String email)
+   {
+      java.util.regex.Pattern VALID_EMAIL_ADDRESS_REGEX = ~/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+      java.util.regex.Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email)
+      return matcher.find()
    }
    
    /**
@@ -66,19 +102,104 @@ class ContactInformation {
     */
    static List removeDuplicates(List contacts)
    {
+      // TODO: keep the record that has more info
       contacts.unique { a, b -> a.email <=> b.email }
+   }
+   
+   static List merge(List contacts1, List contacts2, boolean remove_duplicates)
+   {
+      def merge = contacts1 + contacts2
+      
+      if (remove_duplicates)
+      {
+         merge = removeDuplicates(merge)
+      }
+      
+      return merge
+   }
+   
+   static List minus(List from, List remove)
+   {
+      return from - remove
    }
    
    /**
     * Takes a list of ContactInformation, and returns a string with the CSV format, one field per column.
     * This is useful to import the contacts in systems like mailchimp.
+    * cols: name and number of column [email: 1, name: 0, lastname: 2] if null by default is: [email: 0, name: 1, lastname: 2]
     */
-   static String toCSV(List contacts)
+   static String toCSV(Map cols, List contacts, boolean include_headers, String csv_separator = ',')
    {
+      // TODO: check that cols is valid
+      
+      if (!cols) cols = [email: 0, name: 1, lastname: 2]
+      
+      // Sort entries by col #
+      cols = cols.sort { it.value }
+      
+      String res = ""
+      
+      if (include_headers)
+      {
+         cols.each { name, index ->
+            res += name + csv_separator
+         }
+         res = res[0..-2] // removes last ,
+         res += System.getProperty("line.separator") // end of current row on csv
+      }
+      
+      contacts.each { contact ->
+         cols.each { name, index ->
+            res += contact."$name" + csv_separator
+         }
+         res = res[0..-2] // removes last ,
+         res += System.getProperty("line.separator") // end of current row on csv
+      }
+      
+      return res
+   }
+   
+   /**
+    * cols: col names and indexes to know how to process the CSV, should be set by the user processing the CSV, they know the columns
+    * csv: a csv formatted stirng
+    * has_headers: to avoid processing the first row if it is the headers
+    */
+   static List fromCSV(Map cols, String csv, boolean has_headers, String csv_separator = ',')
+   {
+      // TODO: check that cols is valid
+      csv = csv.denormalize() // csv with end of line of the platform
+      
+      def lines = csv.split(System.getProperty("line.separator"))
+      def res = []
+      def tokens
+      def contact
+      lines.each { line ->
+      
+         //println "l> "+ line +"<l"
+      
+         if (has_headers) // avoid first loop
+         {
+            has_headers = false
+            return
+         }
+      
+         tokens = line.split(csv_separator)*.trim() // gets columns and removes whitespaces from each column value
+         
+         //println "t> "+ tokens +"<t"
+         
+         contact = new ContactInformation()
+         cols.each { name, index ->
+            // names that have spaces in the CSV, have double quotes, the replaceAll removes those quotes
+            contact."$name" = tokens[index].replaceAll('"','') // e.g. contact.email = value on colun $index
+         }
+         res << contact
+      }
+      
+      return res
    }
    
    String toString()
    {
-      this.email +" "+ this.name +" "+ this.lastname
+      this.email +" "+ this.name +" "+ this.lastname +System.getProperty("line.separator")
    }
 }
--- Tests.groovy
+++ Tests.groovy
@@ -1,22 +1,85 @@
 package contact_services
 
-File sourceFile = new File("C:/Documents and Settings/pab/My Documents/GitHub/contact-services/ContactInformation.groovy")
+def path = "C:/Documents and Settings/pab/My Documents/GitHub/contact-services/"
+
+File sourceFile = new File(path + "ContactInformation.groovy")
 Class groovyClass = new GroovyClassLoader(getClass().getClassLoader()).parseClass(sourceFile);
-GroovyObject myObject = (GroovyObject) groovyClass.newInstance();
+GroovyObject contactInfo = (GroovyObject) groovyClass.newInstance();
 
-def test1 = $/
-"abreu.a@gmail.com" <Abreu.a@gmail.com>,
-"adson.d@saude.gov.br" <adson.d@saude.gov.br>,
-"Alvarez Perez, Oscar" <oalvarezb@abcd.es>,
+/*
+// TEST 1
 
-"Andrés F. Sopoao Caral" <fdfd@fff.com.co>,
-asdfffdadfasdf@gmail.com,
-Carlos López Flores <car@fdsf.cl>,
-Roberto Saez <fdsfsdfsdf@gmail.com>,
-Roberto <hjkh@gmail.com>,
-/$
+def test1 = """abreu.gerardo@gmail.com <Abreu.gerardo@gmail.com>
+adson.franca@saude.gov.br <adson.franca@saude.gov.br>
+Alvarez Buron Oscar <oalvarezb@saludcastillayleon.es>
+AMSJ - Miguel P. Diaz Hernandez <mpdh@amsj.com.uy>
+Andrés F. Zapata Osorio <azapata@linkdx.com.co>
+beatriz@hc.ufmg.br <beatriz@hc.ufmg.br>
+cbrown@genexusconsulting.com <cbrown@genexusconsulting.com>
+Chamorro Gordejuela Jacobo <jchamorrog@saludcastillayleon.es>
+Cisneros Martin M Angeles <mcisnerosm@saludcastillayleon.es>
+clarecicardoso@yahoo.com.br <clarecicardoso@yahoo.com.br>
+cleinaldocosta@uol.com.br <cleinaldocosta@uol.com.br>
+D' Agostino Mr. Marcelo (WDC) <dagostim@paho.org>
+"""
 
 // tiene 7 partes genera problemas "AMSJ - Miguel P. Lopez Perez" <asdf@rrrrr.com.uy>,
 
+def contactsss = contactInfo.getContactInformationFromString(test1)
+println contactsss
+*/
 
-myObject.getContactInformationFromString(test1)
+
+
+// TEST 2
+
+File csv1_file = new File(path + "VER QUE NO ESTEN YA EN EX-ALUMN.csv")
+def contacts = contactInfo.getContactInformationFromString(csv1_file.text)
+println contacts.size() + " contacts 1"
+
+File csv2_file = new File(path + "Contactos para difusión de cursos - Sheet1.csv")
+def contacts2 = contactInfo.getContactInformationFromString(csv2_file.text)
+println contacts2.size() + " contacts 2"
+
+def contacts3 = contactInfo.merge(contacts, contacts2, true)
+
+println contacts3.size() + " contacts 3"
+
+def csv = contactInfo.toCSV([email: 0, name: 1, lastname: 2], contacts3, true, ',') // in spanish the csv sep is : in english it's ,
+
+def out = new File(path + "out.csv")
+out.withWriter('UTF-8') { it << csv }
+
+println ""
+println ""
+
+
+
+// TEST 3
+
+// read the output csv to test loading
+def in_csv = out.text
+def imported_contacts = contactInfo.fromCSV([email: 0, name: 1, lastname: 2], in_csv, true, ',') // in spanish the csv sep is : in english it's ,
+//println imported_contacts[0..30]
+
+
+
+// TEST 4 load mailchimp csv
+def in_csv2 = new File(path + "mailchimp_openehr_ex_alumnos.csv").text
+def imported_mailchimp = contactInfo.fromCSV([email: 0, name: 1, lastname: 2], in_csv2, true, ',') // in spanish the csv sep is : in english it's ,
+//println imported_mailchimp[0..30]
+
+def in_csv3 = new File(path + "mailchimp_isis_cbdc_ex_alumnos.csv").text
+def imported_mailchimp2 = contactInfo.fromCSV([email: 0, name: 1, lastname: 2], in_csv3, true, ',') // in spanish the csv sep is : in english it's ,
+
+
+// TEST 5 my contacts minus the mailchimp contacts
+def sent_to = contactInfo.merge(imported_mailchimp2, imported_mailchimp, true) // merges 2 lists that I already sent emails to, with duplicates removed
+def yet_not_sent1 = contactInfo.minus(imported_contacts, sent_to) // my new list minus the emails I already sent
+def yet_not_sent1_csv = contactInfo.toCSV([email: 0, name: 1, lastname: 2], yet_not_sent1, true, ',') // in spanish the csv sep is : in english it's ,
+def yet_not_sent1_out = new File(path + "yet_not_sent1.csv")
+
+yet_not_sent1_out.withWriter('UTF-8') { it << yet_not_sent1_csv }
+
+println ""
+println ""
